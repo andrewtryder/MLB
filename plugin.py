84d0fd5099cae6c506baa2987bf06716bf6dfde9
spline <reticulatingspline@github.com>
2014-08-12 11:59:59 -0400

    Fix mlbplayercontract race condition.

--- plugin.py
+++ plugin.py
@@ -2194,9 +2194,13 @@ class MLB(callbacks.Plugin):
         if not div:
             irc.reply("ERROR: I could not find player contract for: {0} at {1}".format(optplayer, url))
             return
-        # we do have stuff. output.
-        irc.reply("{0} :: {1}".format(self._bold(plrname), div.getText().encode('utf-8')))
-    
+        # race condition here discovered by someone:
+        parentdiv = div.findParent('div')
+        if parentdiv['class'] == "playercard":
+            irc.reply("{0} :: {1}".format(self._bold(plrname), div.getText().encode('utf-8')))
+        else:
+            irc.reply("{0} :: I'm sorry but no contract details are listed on: {1}".format(self._bold(plrname), url))
+
     mlbplayercontract = wrap(mlbplayercontract, [('text')])
     
     def mlbplayernews(self, irc, msg, args, optplayer):
