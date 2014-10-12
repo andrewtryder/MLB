03816400a2b1aa9a776883dd53f5ee5ff8e2b206
spline <reticulatingspline@github.com>
2014-10-12 15:35:55 -0400

    Add initial tests.

--- test.py
+++ test.py
@@ -9,6 +9,20 @@ from supybot.test import *
 
 class MLBTestCase(PluginTestCase):
     plugins = ('MLB',)
-
+    
+    def testMLB(self):
+        # milbplayerinfo, milbplayerseason, mlballstargame, mlbarrests, mlbawards,
+        # mlbbox, mlbcareerleaders, mlbcareerstats, mlbchanlineup, mlbcountdown, mlbcyyoung,
+        # mlbdailyleaders, mlbejections, mlbgamesbypos, mlbgamestats, mlbgameumps,
+        # mlbhittingstreaks, mlbinjury, mlbleaders, mlblineup, mlbmanager, mlbpayroll, mlbpitcher,
+        # mlbplayercontract, mlbplayerinfo, mlbplayernews, mlbplayoffs, mlbpowerrankings, mlbprob,
+        # mlbremaining, mlbroster, mlbrosterstats, mlbschedule, mlbseasonstats, mlbseries,
+        # mlbstandings, mlbteamleaders, mlbteams, mlbteamtrans, mlbtrans, mlbvaluations,
+        # mlbweather, and mlbworldseries
+        self.assertNotError('milbplayerinfo Mike Trout')
+        self.assertNotError('milbplayerseason 2010 mike trout')
+        self.assertNotError('@mlballstargame 2013')
+        self.assertNotError('mlbarrests')
+        self.assertNotError('mlbawards 2013')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
